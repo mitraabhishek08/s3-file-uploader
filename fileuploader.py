51578703311ffe72c75c74018f78d258ab599a1c
Abhishek <mitraabhishek.08@gmail.com>
2025-07-22 11:06:24 +0530

    Update fileuploader.py
    help

--- fileuploader.py
+++ fileuploader.py
@@ -92,7 +92,9 @@ def main():
 
 
     # Mandatory folder input
-    folder_name = st.text_input(f"Folder name under '{BUCKET_NAME}/{BASE_FOLDER}' (required):").strip()
+    folder_name = st.text_input(label=f"Folder name under '{BUCKET_NAME}/{BASE_FOLDER}'",
+    help="This folder name will reuse an existing folder if present, or create a new one.",
+    key="folder_name_input").strip()
     st.info("This folder name will reuse an existing folder if present, or create a new one.")
 
     uploaded_files = st.file_uploader(
